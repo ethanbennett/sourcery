c28820209e4c3346ca1fa0ca8a3dbc2187358663
David Knott <david1k1nott@gmail.com>
2017-04-03 18:52:53 -0600

    Create checkpoints get functionality
    [#142734053]

--- contracts/PathsDB.sol
+++ contracts/PathsDB.sol
@@ -4,11 +4,26 @@ contract PathsDB {
 
     address[] public path;
 
-    function addCheckpoint(address checkpoint) returns (bool){
-      if (checkpoint == 0x0 || path[path.length-1] == checkpoint) {
+    function addCheckpoint(address checkpoint) returns (bool) {
+      if (checkpoint == 0x0) {
         return false;
       }
-      path.push(checkpoint);
-      return true;
+      if (path.length == 0 || path[path.length-1] != checkpoint) {
+        path.push(checkpoint);
+        return true;
+      }
+      return false;
+    }
+
+    function getPath() constant returns (address[]) {
+      uint length = path.length;
+
+      address[] memory checkpoints = new address[](length);
+
+      for (uint i = 0; i < path.length; i++) {
+        checkpoints[i] = path[i];
+      }
+
+      return checkpoints;
     }
 }
