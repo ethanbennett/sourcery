fcace4afc10c7d6b2f3bfed87117e9c35dcfb006
David Knott <david1k1nott@gmail.com>
2017-04-06 10:52:21 -0600

    Create and pass doug is initialized with necesary contracts test
    [#142735169]

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,13 @@
 var PathsDB = artifacts.require("./PathsDB.sol");
 var PathsController = artifacts.require("./PathsController.sol");
+var PermissionsDB = artifacts.require("./PermissionsDB.sol");
+var PermissionsController = artifacts.require("./PermissionsController.sol");
+var Doug = artifacts.require("./Doug.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(PathsDB);
   deployer.deploy(PathsController);
+  deployer.deploy(PermissionsDB);
+  deployer.deploy(PermissionsController);
+  deployer.deploy(Doug);
 };
